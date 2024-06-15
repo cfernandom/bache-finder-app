a662faaf6963ad0b105a0b68a6572aad3b7468c6
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-06-14 20:21:25 -0500

    feat(middleware): define auth_middleware

---
+++ lib/features/auth/middlewares/auth_middleware.dart
@@ -0,0 +1,15 @@
+import 'package:bache_finder_app/features/auth/controllers/auth_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class AuthMiddleware extends GetMiddleware {
+
+  @override
+  RouteSettings? redirect(String? route) {
+    final AuthController authController = Get.find();
+    if (!authController.isAuth.value) {
+      return const RouteSettings(name: '/login');
+    }
+    return null;
+  }
+}
