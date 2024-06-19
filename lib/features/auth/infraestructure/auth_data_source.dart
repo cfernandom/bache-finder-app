89441179b365e27525a7769cd96b417d3ba99cd5
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-06-18 23:35:31 -0500

    feat: agregar AuthDataSource para manejo de autenticación
    - Se ha creado la clase `AuthDataSource` para gestionar las operaciones de autenticación.
    - Se configuró Dio con las opciones base para las solicitudes HTTP.
    - Se implementaron los métodos `login`, `validateSession` y `logout` para manejar el inicio de sesión, la validación de sesión y el cierre de sesión del usuario.
    - Se añadieron métodos privados para gestionar el almacenamiento local del token de autenticación utilizando `StorageService`.
    
    Este cambio introduce la fuente de datos para la autenticación en la aplicación, permitiendo realizar solicitudes HTTP al API de Bache Finder y gestionar el estado de autenticación del usuario de manera eficiente.

---
+++ lib/features/auth/infraestructure/auth_data_source.dart
@@ -0,0 +1,78 @@
+import 'package:bache_finder_app/core/constants/enviroment.dart';
+import 'package:bache_finder_app/features/auth/infraestructure/models/user_model.dart';
+import 'package:bache_finder_app/features/shared/services/storage_service.dart';
+import 'package:dio/dio.dart';
+
+class AuthDataSource {
+  final Dio dio;
+  final StorageService storageService;
+
+  AuthDataSource({
+    required this.dio,
+    required this.storageService,
+  }) {
+    dio.options.baseUrl = Enviroment.bacheFinderApiUrl();
+    dio.options.headers['Content-Type'] = 'application/json';
+    dio.options.headers['Accept'] = 'application/json';
+  }
+
+  Future<UserModel> login(String email, String password) async {
+    try {
+      final response = await dio.post(
+        'v1/login',
+        data: {'email': email, 'password': password},
+      );
+      _setLocalToken(response.data['data']['token']);
+
+      return UserModel.fromJson(response.data['data']);
+    } on DioException catch (e) {
+      throw Exception(e.response?.data['message']);
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+
+  Future<UserModel?> validateSession() async {
+    try {
+      final token = await _getLocalToken();
+      if (token == null) {
+        return null;
+      }
+      dio.options.headers['Authorization'] = 'Bearer $token';
+      final response = await dio.get('v1/user');
+      return UserModel.fromJson(response.data['data']);
+    } on DioException catch (e) {
+      throw Exception(e.response?.data['message']);
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+
+  Future<void> logout() async {
+    try {
+      final token = await _getLocalToken();
+      if (token == null) {
+        return;
+      }
+      dio.options.headers['Authorization'] = 'Bearer $token';
+      await dio.post('v1/logout');
+      _deleteLocalToken();
+    } on DioException catch (e) {
+      throw Exception(e.response?.data['message']);
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+
+  Future<String?> _getLocalToken() async {
+    return await storageService.getValueByKey('token');
+  }
+
+  Future<void> _deleteLocalToken() async {
+    await storageService.deleteValueByKey('token');
+  }
+
+  Future<void> _setLocalToken(String token) async {
+    await storageService.setKeyValue('token', token);
+  }
+}
