185384aa42fd3cb663b2c682909c6359e464585f
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-06-18 23:41:04 -0500

    refactor(auth): actualizar AuthController

---
+++ lib/features/auth/presentation/controllers/auth_controller.dart
@@ -0,0 +1,56 @@
+import 'package:bache_finder_app/features/auth/domain/entities/user.dart';
+import 'package:bache_finder_app/features/auth/domain/use_cases/login.dart';
+import 'package:bache_finder_app/features/auth/domain/use_cases/logout.dart';
+import 'package:bache_finder_app/features/auth/domain/use_cases/validate_session.dart';
+import 'package:get/get.dart';
+
+class AuthController extends GetxController {
+  final Login loginUseCase;
+  final Logout logoutUseCase;
+  final ValidateSession validateSessionUseCase;
+
+  AuthController({
+    required this.loginUseCase,
+    required this.logoutUseCase,
+    required this.validateSessionUseCase,
+  });
+
+  var user = Rxn<User>();
+  var isLoading = true.obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    validateSession();
+  }
+
+  Future<bool> validateSession() async {
+    isLoading.value = true;
+    final result = await validateSessionUseCase.call();
+    result.fold(
+      (failure) => print(failure),
+      (user) => this.user.value = user,
+    );
+    isLoading.value = false;
+
+    return result.isRight();
+  }
+
+  Future<bool> login(String email, String password) async {
+    isLoading.value = true;
+    final result = await loginUseCase.call(email, password);
+    result.fold(
+      (failure) => print(failure),
+      (user) => this.user.value = user,
+    );
+    isLoading.value = false;
+
+    return result.isRight();
+  }
+
+  Future<void> logout() async {
+    isLoading.value = true;
+    await logoutUseCase.call();
+    isLoading.value = false;
+  }
+}
