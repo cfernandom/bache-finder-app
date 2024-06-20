784cf8bc88e8840c9c4855080bcec7cf49542d07
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-06-19 23:49:39 -0500

    refactor: update AuthRepository

--- lib/features/auth/domain/repositories/auth_repository.dart
+++ lib/features/auth/domain/repositories/auth_repository.dart
@@ -2,7 +2,7 @@ import 'package:bache_finder_app/features/auth/domain/entities/user.dart';
 import 'package:fpdart/fpdart.dart';
 
 abstract class AuthRepository {
-  Future<Either<Exception, User>> login(String email, password);
+  Future<Either<Exception, User>> login(String email, String password);
   Future<Either<Exception, void>> logout();
   Future<Either<Exception, User?>> validateSession();
 }
