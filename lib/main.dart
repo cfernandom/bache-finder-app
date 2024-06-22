28f4c1cbd588e8ac31095f334a95f55f21f89c12
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-06-21 22:02:51 -0500

    feat(binding): asegurarse de que los bindings sean cargados antes de iniciar la aplicación

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Enviroment.initEnviroment();
   runApp(const MyApp());
 }
