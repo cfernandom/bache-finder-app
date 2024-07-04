01656c2b7dc1b3f045c08fb533149d5326ecdb23
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-07-03 23:28:34 -0500

    feat(widget): crear image_viewer_widget para mostra imagen local o remota

---
+++ lib/features/shared/presentation/widgets/image_viewer_widget.dart
@@ -0,0 +1,38 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+class ImageViewerWidget extends StatelessWidget {
+  final String image;
+
+  const ImageViewerWidget(this.image, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    if (image == '') {
+      return ClipRRect(
+        borderRadius: BorderRadius.circular(8),
+        child: Image.asset(
+          'assets/images/no-image.jpg',
+          fit: BoxFit.cover,
+          height: 250,
+        ),
+      );
+    }
+
+    late ImageProvider imageProvider;
+    if (image.startsWith('http')) {
+      imageProvider = NetworkImage(image);
+    } else {
+      imageProvider = FileImage(File(image));
+    }
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(8),
+      child: FadeInImage(
+        image: imageProvider,
+        fit: BoxFit.cover,
+        placeholder: const AssetImage('assets/images/Spinner-1s-264px.gif'),
+      ),
+    );
+  }
+}
