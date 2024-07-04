785bd6447e500a5bc4dc81a0ec750d46a62904bf
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-07-03 23:30:13 -0500

    feat(input): crear image_input para validar campos de imagen

---
+++ lib/features/shared/infrastructure/inputs/image_input.dart
@@ -0,0 +1,27 @@
+import 'package:formz/formz.dart';
+
+enum ImageValidationError { empty, format }
+
+class ImageInput extends FormzInput<String, ImageValidationError> {
+  const ImageInput.pure() : super.pure('');
+  const ImageInput.dirty(super.value) : super.dirty();
+  
+  @override
+  ImageValidationError? validator(String value) {
+    if (value.isEmpty || value.trim().isEmpty) {
+      return ImageValidationError.empty;
+    }
+    if (!value.endsWith('.jpg') && !value.endsWith('.jpeg') && !value.endsWith('.png')) {
+      return ImageValidationError.format;
+    }
+    return null;
+  }
+
+  String? get errorMessage {
+    final error = this.error;
+    if (error == null) return null;
+    if (error == ImageValidationError.empty) return 'La imagen es obligatoria';
+    if (error == ImageValidationError.format) return 'El formato de la imagen no es valido.\nSolo se aceptan imagenes .jpg, .jpeg o .png';
+    return null;
+  }
+}
