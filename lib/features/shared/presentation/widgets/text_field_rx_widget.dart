56da327f843f3e989a801f2201b8fc9a17408b0d
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-07-13 23:34:08 -0500

    feat(widget): crear campo de texto stateful

---
+++ lib/features/shared/presentation/widgets/text_field_rx_widget.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class TextFieldRxWidget extends StatefulWidget {
+  const TextFieldRxWidget({
+    super.key, 
+    required this.label,
+    this.initialValue = '',
+    this.isObscure = false,
+    this.keyboardType = TextInputType.text,
+    this.errorMessage,
+    this.onChanged,
+    this.prefix = '',
+    this.suffix = '',
+  });
+
+  final String label;
+  final String initialValue;
+  final bool isObscure;
+  final TextInputType keyboardType;
+  final String? errorMessage;
+  final void Function(String)? onChanged;
+  final String prefix;
+  final String suffix;
+
+  @override
+  State<TextFieldRxWidget> createState() => _TextFieldRxWidgetState();
+}
+
+class _TextFieldRxWidgetState extends State<TextFieldRxWidget> {
+  final _controller = TextEditingController();
+
+  @override
+  void initState() {
+    _controller.text = widget.initialValue;
+    super.initState();
+  }
+
+  @override
+  void didUpdateWidget(covariant TextFieldRxWidget oldWidget) {
+    _controller.text = widget.initialValue;
+    super.didUpdateWidget(oldWidget);
+  }
+
+   @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: _controller,
+      obscureText: widget.isObscure,
+      keyboardType: widget.keyboardType,
+      onChanged: widget.onChanged,
+      decoration: InputDecoration(
+        labelText: widget.label,
+        prefixText: widget.prefix,
+        suffixText: widget.suffix,
+        errorText: widget.errorMessage,
+      ),
+    );
+  }
+}
