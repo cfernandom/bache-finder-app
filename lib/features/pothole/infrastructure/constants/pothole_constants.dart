0170656c0bd24107e79b98b2e7607baf46000022
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-08-02 22:50:19 -0500

    chore: add PotholeConstants

---
+++ lib/features/pothole/infrastructure/constants/pothole_constants.dart
@@ -0,0 +1,16 @@
+class PotholeConstants {
+  static const List<String> types = [
+    'No definido',
+    'Bache',
+    'Descascaramiento',
+    'Fisura en bloque',
+    'Fisura por deslizamiento',
+    'Fisura por reflexión',
+    'Fisuras longitudinales y transversales',
+    'Fisura transversal',
+    'Hundimiento',
+    'Parche',
+    'Pérdida de agregado',
+    'Piel de cocodrilo',
+  ];
+}
