f58849bc157f8a17af8e1bc24ab175b5adecfeb1
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-08-21 09:27:35 -0500

    feat(ui):  ajustes de estilo del campo de texto

--- lib/features/shared/presentation/widgets/text_field_rx_widget.dart
+++ lib/features/shared/presentation/widgets/text_field_rx_widget.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class TextFieldRxWidget extends StatefulWidget {
   const TextFieldRxWidget({
-    super.key, 
+    super.key,
     required this.label,
     this.initialValue = '',
     this.isObscure = false,
@@ -43,7 +43,7 @@ class _TextFieldRxWidgetState extends State<TextFieldRxWidget> {
     super.didUpdateWidget(oldWidget);
   }
 
-   @override
+  @override
   void dispose() {
     _controller.dispose();
     super.dispose();
@@ -53,16 +53,33 @@ class _TextFieldRxWidgetState extends State<TextFieldRxWidget> {
   Widget build(BuildContext context) {
     return TextFormField(
       maxLines: widget.maxLines,
+      textAlign: TextAlign.left,
+      textAlignVertical: TextAlignVertical.center,
       minLines: 1,
       controller: _controller,
       obscureText: widget.isObscure,
       keyboardType: widget.keyboardType,
       onChanged: widget.onChanged,
+      style: Theme.of(context).textTheme.bodyMedium,
       decoration: InputDecoration(
-        labelText: widget.label,
         prefixText: widget.prefix,
         suffixText: widget.suffix,
         errorText: widget.errorMessage,
+        floatingLabelBehavior: FloatingLabelBehavior.never,
+        contentPadding: const EdgeInsets.fromLTRB(8.0, -8.0, 8.0, 8.0),
+        isDense: true,
+        label: Text(widget.label,
+            style: Theme.of(context)
+                .textTheme
+                .bodyMedium
+                ?.copyWith(color: const Color(0xFFBDBDBD))),
+        border: const UnderlineInputBorder(
+          borderSide: BorderSide(color: Color(0xFFB9E1F2), width: 1.0),
+          borderRadius: BorderRadius.all(Radius.circular(8.0)),
+        ),
+        enabledBorder: InputBorder.none,
+        fillColor: const Color(0xFFFFFFFF),
+        filled: true,
       ),
     );
   }
