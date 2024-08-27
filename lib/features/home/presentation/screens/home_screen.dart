00f41e8fbe7c6ef029dd238e05ac1707597f8934
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-08-27 08:07:44 -0500

    feat(ui): actualizar estilo de homeScreen

--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:bache_finder_app/core/router/app_pages.dart';
 import 'package:bache_finder_app/features/auth/presentation/controllers/session_controller.dart';
+import 'package:bache_finder_app/features/shared/presentation/widgets/gap_widget.dart';
+import 'package:bache_finder_app/features/shared/presentation/widgets/outlined_button_icon_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:go_router/go_router.dart';
@@ -10,33 +12,86 @@ class HomeScreen extends GetView<SessionController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xFFE8F5FB),
       appBar: AppBar(
-        title: const Text('Home Page'),
+        title: Text('Bache Finder App', style: Theme.of(context).textTheme.titleLarge!.copyWith(color: Colors.white)),
+        backgroundColor: const Color(0xFF2C5461),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                context.push('${AppPaths.potholes}/all');
-              },
-              child: const Text('Mostrar Baches'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                context.push(AppPaths.profile);
-              },
-              child: const Text('Ir a mi perfil'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                controller.logout();
-              },
-              child: const Text('Cerrar sesión'),
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              // ElevatedButton(
+              //   onPressed: () {
+              //     context.push(AppPaths.profile);
+              //   },
+              //   child: const Text('Ir a mi perfil'),
+              // ),
+              const GapWidget(size: 8),
+              SizedBox(
+                height: 48,
+                child: OutlinedButtonIconWidget(
+                  onPressed: () {
+                    context.push('${AppPaths.potholes}/all');
+                  },
+                  label: 'Baches reportados',
+                  icon: Icons.list_alt,
+                ),
+              ),
+              const GapWidget(size: 8),
+              SizedBox(
+                height: 48,
+                child: OutlinedButtonIconWidget(
+                  onPressed: () {
+                    context.push('${AppPaths.potholes}/new');
+                  },
+                  label: 'Reportar bache',
+                  icon: Icons.add,
+                ),
+              ),
+              const GapWidget(size: 8),
+              ElevatedButtonWidget(
+                onPressed: () {
+                  controller.logout();
+                },
+                label: 'Cerrar sesión',
+              ),
+              // ElevatedButton(
+              //   onPressed: () {
+              //     context.go(AppPaths.maps);
+              //   },
+              //   child: const Text('Ir a Maps'),
+              // ),
+            ],
+          ),
         ),
+      ),
+    );
+  }
+}
+
+class ElevatedButtonWidget extends StatelessWidget {
+  const ElevatedButtonWidget(
+      {super.key, required this.onPressed, required this.label});
+
+  final String label;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      height: 48,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(8)),
+          ),
+        ),
+        onPressed: onPressed,
+        child: Text(label),
       ),
     );
   }
