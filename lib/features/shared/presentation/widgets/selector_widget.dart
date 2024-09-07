42ed2d2ce3339aac23cd5abc094d515b9fa3a8a3
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-09-07 18:20:28 -0500

    feat(ui): actualizar estilo selector

--- lib/features/shared/presentation/widgets/selector_widget.dart
+++ lib/features/shared/presentation/widgets/selector_widget.dart
@@ -2,27 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class SelectorWidget extends StatelessWidget {
-  final void Function(String) onChanged;
+  final void Function(String)? onChanged;
   final String initialValue;
   final List<String> items;
+  final IconData icon;
 
   const SelectorWidget({
     super.key,
     required this.onChanged,
     required this.initialValue,
     required this.items,
+    required this.icon,
   });
 
   @override
   Widget build(BuildContext context) {
     final controller = Get.put(SelectorController());
-    
+
     controller.selectedValue.value = initialValue;
 
     return Container(
       padding: const EdgeInsets.all(8.0),
       decoration: BoxDecoration(
-        color: const Color(0xFFFFFFFF),
+        color: const Color(0xFFE8F5FB),
         borderRadius: BorderRadius.circular(8.0),
       ),
       child: Obx(() {
@@ -32,16 +34,21 @@ class SelectorWidget extends StatelessWidget {
           value: controller.selectedValue.value,
           isExpanded: true,
           underline: const SizedBox.shrink(),
+          icon: Icon(
+            icon,
+          ),
           items: items.map((String item) {
             return DropdownMenuItem<String>(
               value: item,
-              child: Text(item, style: Theme.of(context).textTheme.bodyMedium),
+              child: Text(item, style: Theme.of(context).textTheme.titleMedium),
             );
           }).toList(),
-          onChanged: (newValue) {
-            controller.updateValue(newValue ?? items.first);
-            onChanged(newValue ?? items.first);
-          },
+          onChanged: onChanged == null
+              ? null
+              : (newValue) {
+                  controller.updateValue(newValue ?? items.first);
+                  onChanged!(newValue ?? items.first);
+                },
         );
       }),
     );
