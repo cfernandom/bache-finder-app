64ee595e9b9ff0afb7204df8853095027e705f76
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-09-24 12:15:24 -0500

    feat(ui): implementar enlaces en drawer

--- lib/features/home/presentation/widgets/home_drawer_widget.dart
+++ lib/features/home/presentation/widgets/home_drawer_widget.dart
@@ -1,10 +1,16 @@
+import 'package:bache_finder_app/core/router/app_pages.dart';
+import 'package:bache_finder_app/features/auth/presentation/controllers/session_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:go_router/go_router.dart';
 
 class HomeDrawerWidget extends StatelessWidget {
   const HomeDrawerWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final SessionController sessionController = Get.find();
+
     return Drawer(
       backgroundColor: const Color(0xFFE8F5FB),
       child: ListView(
@@ -41,21 +47,21 @@ class HomeDrawerWidget extends StatelessWidget {
             icon: const Icon(Icons.person, color: Color(0xFF2C5461)),
             label: 'Mi Perfil',
             onTap: () {
-              // Navega a la página de perfil
+              context.push(AppPaths.profile);
             },
           ),
           _ListTileWidget(
             icon: const Icon(Icons.view_list, color: Color(0xFF2C5461)),
             label: 'Mis Reportes',
             onTap: () {
-              // Navega a la página de reportes
+              context.push('${AppPaths.potholes}/all');
             },
           ),
           _ListTileWidget(
             icon: const Icon(Icons.add_circle, color: Color(0xFF2C5461)),
             label: 'Nuevo Reporte',
             onTap: () {
-              // Navega a la página de nuevo reporte
+              context.push('${AppPaths.potholes}/new');
             },
           ),
           _ListTileWidget(
@@ -69,14 +75,14 @@ class HomeDrawerWidget extends StatelessWidget {
             icon: const Icon(Icons.info, color: Color(0xFF2C5461)),
             label: 'Información',
             onTap: () {
-              // Navega a la página de información
+              context.push(AppPaths.about);
             },
           ),
           _ListTileWidget(
             icon: const Icon(Icons.logout, color: Color(0xFF2C5461)),
             label: 'Cerrar sesión',
             onTap: () {
-              // Realiza la acción de cerrar sesión
+              sessionController.logout();
             },
           )
         ],
